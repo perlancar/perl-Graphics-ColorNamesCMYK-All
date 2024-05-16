257ee62344586cde8f857617223eb137833a439f
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2024-05-16 22:07:06 +0700

    Initial commit

---
+++ lib/Graphics/ColorNamesCMYK/All.pm
@@ -0,0 +1,47 @@
+package Graphics::ColorNamesCMYK::All;
+
+use strict 'vars', 'subs';
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+# BEGIN_CODE
+my @modules = qw(
+    Graphics::ColorNamesCMYK::BannersCom
+    Graphics::ColorNamesCMYK::JohnDecemberCom
+    Graphics::ColorNamesCMYK::Pantone
+    Graphics::ColorNamesCMYK::ToutesLesCouleursCom::Black
+    Graphics::ColorNamesCMYK::ToutesLesCouleursCom::Blue
+    Graphics::ColorNamesCMYK::ToutesLesCouleursCom::Brown
+    Graphics::ColorNamesCMYK::ToutesLesCouleursCom::Gray
+    Graphics::ColorNamesCMYK::ToutesLesCouleursCom::Green
+    Graphics::ColorNamesCMYK::ToutesLesCouleursCom::Orange
+    Graphics::ColorNamesCMYK::ToutesLesCouleursCom::Pink
+    Graphics::ColorNamesCMYK::ToutesLesCouleursCom::Purple
+    Graphics::ColorNamesCMYK::ToutesLesCouleursCom::Red
+    Graphics::ColorNamesCMYK::ToutesLesCouleursCom::White
+    Graphics::ColorNamesCMYK::ToutesLesCouleursCom::Yellow
+);
+
+my $table;
+for my $module (@modules) {
+    (my $modulepm = "$module.pm") =~ s!::!/!g;
+    require $modulepm;
+    for keys (%{ "$module\::$NAMES_CMYK_TABLE" }) { $table->{$_} = ${"$module\::$NAMES_CMYK_TABLE"}->{$_} }
+}
+
+use Data::Dump;
+print 'our $NAMES_CMYK_TABLE = ', Data::Dump::dump($table), ";\n";
+# END_CODE
+
+1;
+# ABSTRACT: CMYK colors from all Graphics::ColorNamesCMYK::*
+
+=head1 DESCRIPTION
+
+
+=head1 SEE ALSO
+
+Other C<Graphics::ColorNamesCMYK::*> modules.
