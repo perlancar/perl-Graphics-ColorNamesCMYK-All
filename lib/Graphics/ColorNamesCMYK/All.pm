b9fde321ffed410bf8823847374d7f97ae5cc7c8
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2024-05-16 22:12:05 +0700

    Release v0.001

--- lib/Graphics/ColorNamesCMYK/All.pm
+++ lib/Graphics/ColorNamesCMYK/All.pm
@@ -8,32 +8,33 @@ use strict 'vars', 'subs';
 # VERSION
 
 # BEGIN_CODE
-my @modules = qw(
-    Graphics::ColorNamesCMYK::BannersCom
-    Graphics::ColorNamesCMYK::JohnDecemberCom
-    Graphics::ColorNamesCMYK::Pantone
-    Graphics::ColorNamesCMYK::ToutesLesCouleursCom::Black
-    Graphics::ColorNamesCMYK::ToutesLesCouleursCom::Blue
-    Graphics::ColorNamesCMYK::ToutesLesCouleursCom::Brown
-    Graphics::ColorNamesCMYK::ToutesLesCouleursCom::Gray
-    Graphics::ColorNamesCMYK::ToutesLesCouleursCom::Green
-    Graphics::ColorNamesCMYK::ToutesLesCouleursCom::Orange
-    Graphics::ColorNamesCMYK::ToutesLesCouleursCom::Pink
-    Graphics::ColorNamesCMYK::ToutesLesCouleursCom::Purple
-    Graphics::ColorNamesCMYK::ToutesLesCouleursCom::Red
-    Graphics::ColorNamesCMYK::ToutesLesCouleursCom::White
-    Graphics::ColorNamesCMYK::ToutesLesCouleursCom::Yellow
-);
-
-my $table;
-for my $module (@modules) {
-    (my $modulepm = "$module.pm") =~ s!::!/!g;
-    require $modulepm;
-    for keys (%{ "$module\::$NAMES_CMYK_TABLE" }) { $table->{$_} = ${"$module\::$NAMES_CMYK_TABLE"}->{$_} }
-}
-
-use Data::Dump;
-print 'our $NAMES_CMYK_TABLE = ', Data::Dump::dump($table), ";\n";
+#no strict 'refs';
+#my @modules = qw(
+#    Graphics::ColorNamesCMYK::BannersCom
+#    Graphics::ColorNamesCMYK::JohnDecemberCom
+#    Graphics::ColorNamesCMYK::Pantone
+#    Graphics::ColorNamesCMYK::ToutesLesCouleursCom::Black
+#    Graphics::ColorNamesCMYK::ToutesLesCouleursCom::Blue
+#    Graphics::ColorNamesCMYK::ToutesLesCouleursCom::Brown
+#    Graphics::ColorNamesCMYK::ToutesLesCouleursCom::Gray
+#    Graphics::ColorNamesCMYK::ToutesLesCouleursCom::Green
+#    Graphics::ColorNamesCMYK::ToutesLesCouleursCom::Orange
+#    Graphics::ColorNamesCMYK::ToutesLesCouleursCom::Pink
+#    Graphics::ColorNamesCMYK::ToutesLesCouleursCom::Purple
+#    Graphics::ColorNamesCMYK::ToutesLesCouleursCom::Red
+#    Graphics::ColorNamesCMYK::ToutesLesCouleursCom::White
+#    Graphics::ColorNamesCMYK::ToutesLesCouleursCom::Yellow
+#);
+#
+#my $table;
+#for my $module (@modules) {
+#    (my $modulepm = "$module.pm") =~ s!::!/!g;
+#    require $modulepm;
+#    for (keys %{${"$module\::NAMES_CMYK_TABLE"}}) { $table->{$_} = ${"$module\::NAMES_CMYK_TABLE"}->{$_} }
+#}
+#
+#use Data::Dump;
+#print 'our $NAMES_CMYK_TABLE = ', Data::Dump::dump($table), ";\n";
 # END_CODE
 
 1;
